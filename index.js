0056ec037aee3eac11b5d9e0949bd34a9277b8d4
Carson Buckley <45154466+CarsonBuckley@users.noreply.github.com>
2022-05-25 12:30:07 -0600

    Jamie's first attack

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+
+// Jons stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+// Jamies stats
+let jamieLannisterAttack = 107;
+
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
@@ -8,4 +15,12 @@ if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jamie Lannister has better attack than Jon Snow");
 } else {
     console.log("Jon Snow and Jamie Lannister have the same attack")
+}
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain.");
+} else {
+    // jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    jonSnowHealth -= jamieLannisterAttack
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
 }
